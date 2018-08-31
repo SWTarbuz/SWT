aac23e7b80efc40a399f8dd23188927b2ff303c5
Due <au268046@uni.au.dk>
2018-08-31 10:56:57 +0200

    1st try

---
+++ Calculator/Calculator/Calculator.Application/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Calculator();
+
+            Console.WriteLine($"Test 5 + 2: {test.Add(5, 2)}");
+            Console.WriteLine($"Test 5 - 2: {test.Subtract(5, 2)}");
+            Console.WriteLine($"Test 5 * 2: {test.Multiply(5, 2)}");
+            Console.WriteLine($"Test 5^2 : {test.Power(5, 2)}");
+        }
+    }
+}
---
+++ Calculator/Calculator/Calculator.Test.Unit/UnitTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.Test.Unit
+{
+    public class UnitTest
+    {
+        private Calculator uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new Calculator();
+        }
+
+        [Test]
+        public void Add_Integers_Test()
+        {
+            Assert.AreEqual(40, uut.Add(23, 17));
+
+        }
+
+        [Test]
+        public void Add_Deci_Test()
+        {
+            Assert.AreEqual(10, uut.Add(5.5, 4.5));
+
+        }
+
+        [Test]
+        public void Multiply_Integers_Test()
+        {
+            Assert.AreEqual(8, uut.Multiply(4, 2));
+
+        }
+
+        [Test]
+        public void Multiply_deci_Test()
+        {
+            Assert.AreEqual(10.5, uut.Multiply(3.5, 3));
+
+        }
+
+        [Test]
+        public void Subtract_Integers_Test()
+        {
+            Assert.AreEqual(23, uut.Subtract(40, 17));
+
+        }
+
+        [Test]
+        public void Power_Integers_Test()
+        {
+            Assert.AreEqual(256, uut.Power(2, 8));
+
+        }
+    }
+}
