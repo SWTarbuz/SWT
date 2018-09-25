877c937f2696e3fe08b5132685ebf48c87c3aaa0
Hamza <Hamzaabdallah82@gmail.com>
2018-09-25 09:08:07 +0200

    handin 2 første udkast

---
+++ Air Traffic Monitoring part 1/Air Traffic Monitoring part 1/TransponderReceiver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransponderReceiver
+{
+    public class RawTransponderDataEventArgs : EventArgs
+    {
+        public RawTransponderDataEventArgs(List<string> transponderData)
+        {
+            TransponderData = transponderData;
+        }
+        public List<string> TransponderData { get; }
+    }
+    public interface ITransponderReceiver
+    {
+        event EventHandler<RawTransponderDataEventArgs> TransponderDataReady;
+    }
+
+    public class TransponderReceiverFactory
+    {
+        public static ITransponderReceiver CreateTransponderDataReceiver { get; }
+    }
+}
