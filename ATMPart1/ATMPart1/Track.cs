09084148d5c16e9837e0e19b4e13013b1abaff2d
Anders Helboe Poulsen <au573698@uni.au.dk>
2018-10-04 23:24:03 +0200

    sepereret funktonalitet i track, og rettet tests til at reflektere dette

--- ATMPart1/ATMPart1/Track.cs
+++ ATMPart1/ATMPart1/Track.cs
@@ -27,44 +27,58 @@ namespace ATMPart1
 
         }
 
+        /// <summary>
+        /// changes position and calculates course and velocity
+        /// </summary>
         public void ChangePosition(float x, float y, float alt, DateTime time)
         {
             TimeSpan diff = time.Subtract(timestamp);
 
             if (diff == TimeSpan.Zero) throw new ArgumentException("datetime unchanged", nameof(time));
 
-            CalcVelocityAndCourse(xPos, x, yPos, y, altitude, alt, diff);
+            //calcDeltaDistances for use by following Functions
+            float[] deltaDistances = CalcDeltaValues(xPos, x, yPos, y, altitude, alt);
 
+            CalcVelocity(deltaDistances[0], deltaDistances[1], deltaDistances[2], diff);
+            CalcCourse(deltaDistances[0], deltaDistances[1]);
+            
+            //change positions
             xPos = x;
             yPos = y;
             this.altitude = alt;
             timestamp = time;
         }
 
-        //would be smart to make a setup function that calculates delta's thus reducing amount of arguments to test for in this and calcCourse
-        void CalcVelocityAndCourse(float oldX, float newX, float oldY, float newY, float oldAlt, float newAlt, TimeSpan deltaT)
+        /// <summary>
+        /// Calculates the delta's on x, y and altitude (z)
+        /// </summary>
+        float[] CalcDeltaValues(float oldX, float newX, float oldY, float newY, float oldAlt, float newAlt)
         {
-            //setup
-            var y = newY - oldY;
-            var x = newX - oldX;
-            var z = newAlt - oldAlt;
+            return new float[3]{(newX-oldX), (newY-oldY), (newAlt-oldAlt)};
+        }
 
-            //calculate velocity in m/s
-            var distance = Math.Sqrt(Math.Pow((double)x, 2 ) + Math.Pow((double)y, 2) + Math.Pow((double)z, 2));
+        //would be smart to make a setup function that calculates delta's thus reducing amount of arguments to test for in this and calcCourse
+        //calculate velocity in m/s
+        void CalcVelocity(float dX, float dY, float dZ, TimeSpan deltaT)
+        {
+            var distance = Math.Sqrt(Math.Pow((double)dX, 2 ) + Math.Pow((double)dY, 2) + Math.Pow((double)dZ, 2));
 
             velocity = (float)((double)distance / (double)deltaT.TotalSeconds);
+        }
 
-            //use pythagoras to find compass course, as the angle between north (y), and c
-            
-            int r = (int)Math.Sqrt(Math.Pow((double)x, 2) + Math.Pow((double)y, 2));
+        //calc course 0 = north, 90 = east, 180 = south 270 = west, and so forth
+        void CalcCourse(float dX, float dY)
+        {
+            int r = (int)Math.Sqrt(Math.Pow((double)dX, 2) + Math.Pow((double)dY, 2));
 
-            
             //se schaum's outline mathmatical handbook for info om Quadrants, bruger dem omvendt grundet vi gerne vil havde den inverse vinkel.
             if (r == 0) return; //if no movement, keep previous direction
-            else if (y >= 0 && x > 0) compassCourse = Math.Abs((int) Math.Round(Math.Asin(x / r) * 360 / (2 * Math.PI))); //Q1
-            else if (y < 0 && x >= 0) compassCourse = 90 + Math.Abs((int) Math.Round(Math.Acos(x / r) * 360 / (2 * Math.PI))); //Q4
-            else if (y <= 0 && x < 0) compassCourse = 180 + Math.Abs((int) Math.Round(Math.Acos(y / r) * 360 / (2 * Math.PI))); //Q3
-            else if (y > 0 && x <= 0) compassCourse = 270 + Math.Abs((int) Math.Round(Math.Asin(y / r) * 360 / (2 * Math.PI))); //Q2
+            else if (dY >= 0 && dX > 0) compassCourse = Math.Abs((int)Math.Round(Math.Asin(dX / r) * 360 / (2 * Math.PI))); //Q1
+            else if (dY < 0 && dX >= 0) compassCourse = 90 + Math.Abs((int)Math.Round(Math.Acos(dX / r) * 360 / (2 * Math.PI))); //Q4
+            else if (dY <= 0 && dX < 0) compassCourse = 180 + Math.Abs((int)Math.Round(Math.Acos(dY / r) * 360 / (2 * Math.PI))); //Q3
+            else if (dY > 0 && dX <= 0) compassCourse = 270 + Math.Abs((int)Math.Round(Math.Asin(dY / r) * 360 / (2 * Math.PI))); //Q2
+
+            if (compassCourse == 360) compassCourse = 0; //fix to ensure values between 0 and 359
         }
     }
 }
--- ATMPart1/ATMUnitTest/TestTrack.cs
+++ ATMPart1/ATMUnitTest/TestTrack.cs
@@ -20,27 +20,33 @@ namespace ATMUnitTest
         {
 
         }
-        //       oldX  newX  oldY   newY    oldTime             newTime         expectedResult
-        [TestCase(0,   1000,  0,    1000, "201712122000250", "201712122001250", 45)] //Q1 45* = 45*
-        [TestCase(0,   2000,  0,    1000, "201712122000250", "201712122021250", 63)] //Q1 63* = 63*
-        [TestCase(0,      0,  1000,    0, "201712122000250", "201712122021250", 180)] //Q4 90* = 180*
-        [TestCase(1000,   0,  0,       0, "201712122000250", "201712122021250", 270)] //Q3 90* = 270*
-        [TestCase(0,      0,  0,    1000, "201712122000250", "201712122021250", 360)] //Q2 90* = 360*
-        public void TestChangePosition_ValidValues_CalculatesCorrectCompassCourse(float oldX, float newX, float oldY, float newY, string oldTime, string newTime, int expectedResult)
+        //doesn't have test to see if ChangePosition changes the properties, except for course and velocity
+        //also lacks tests to see that illegal values are handled correctly
+
+        /// <summary>
+        /// Test of calls to CalcCourse from ChangePosition
+        /// </summary>
+        //         dX     dY    expectedResult
+        [TestCase(1000,   1000, 45)] //Q1 45* = 45*
+        [TestCase(2000,   1000, 63)] //Q1 63* = 63*
+        [TestCase(0,     -1000, 180)] //Q4 90* = 180*
+        [TestCase(-1000,   0,   270)] //Q3 90* = 270*
+        [TestCase(0,      1000, 0)] //Q2 90* = 360* = 0* due to this being the same and design says 0* to 359*
+        public void TestChangePosition_CalcCourse_CalculatesExpectedValue(float dX, float dY, int expectedResult)
         {
-            //Arrange
-            string[] formats = { "yyyyMMddHHmmfff" };
-            var time1 = DateTime.ParseExact(oldTime, formats[0], CultureInfo.CurrentCulture);
-            var time2 = DateTime.ParseExact(newTime, formats[0], CultureInfo.CurrentCulture);
+            DateTime time = DateTime.Now.Add(new TimeSpan(0, 1, 0, 0));
 
-            var UUT = new Track("", oldX, oldY, 0, time1);
+            var UUT = new Track("", 0, 0, 0, DateTime.Now);
 
-            UUT.ChangePosition(newX, newY, 0, time2); //act
+            UUT.ChangePosition(dX, dY, 0, time); //act
 
             Assert.That(UUT.compassCourse, Is.EqualTo(expectedResult));
 
         }
 
+        /// <summary>
+        /// Test of calls to CalcVelocity from ChangePosition
+        /// </summary>
         //       oldX  newX  oldY   newY  oldZ  newZ     oldTime         newTime       expectedResult
         [TestCase(2000, 3000, 2000, 3000, 2000, 3000, "201712122000250", "201712122001250", 28.8675134f)] //positive vals 1 min
         [TestCase(3000, 2000, 3000, 2000, 3000, 2000, "201712122000250", "201712122100250", 0.481125f)] //negative vals & 1 hour
@@ -48,7 +54,7 @@ namespace ATMUnitTest
         [TestCase(2000, 3000, 2000, 3000, 2000, 3000, "201712122000250", "201712122000251", 1732050.807569f)] //1000'th of a second positive vals
         [TestCase(2000, 2000, 2000, 2000, 2000, 2000, "201712122000250", "201712122000251", 0f)] //no location difference, 1000th of a second
         //[TestCase(2000, 3000, 2000, 3000, 2000, 3000, "201712122000250", "201712122000251", 1732050.807569f)] //no time difference positive vals (expects time rounding)
-        public void TestChangePosition_ValidValues_CalculatesCorrectVelocity(float oldX, float newX, float oldY, float newY, float oldZ, float newZ, string oldTime, string newTime, float expectedResult)
+        public void TestChangePosition_CalcVelocity_CalculatesExpectedValue(float oldX, float newX, float oldY, float newY, float oldZ, float newZ, string oldTime, string newTime, float expectedResult)
         {
             //Arrange
             string[] formats = { "yyyyMMddHHmmfff" };
