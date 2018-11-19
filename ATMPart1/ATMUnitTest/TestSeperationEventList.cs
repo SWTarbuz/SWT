80d28166f8c6b1db846d281b00354bfee815fcef
Anders Helboe Poulsen <au573698@uni.au.dk>
2018-11-19 11:25:22 +0100

    Added Test For SeperationEventList, With test: EndEvent_EventExists_RemovesEvent

---
+++ ATMPart1/ATMUnitTest/TestSeperationEventList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATMPart1;
+using NSubstitute;
+using NUnit.Framework;
+using TransponderReceiver;
+
+namespace ATMUnitTest
+{
+    class TestSeperationEventList
+    {
+        private ISeperationEventList _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new SeperationEventList();
+        }
+
+        //MethodUnderTest_Scenario_Behaviour
+        [Test]
+        public void EndEvent_EventExists_RemovesEvent()
+        {
+            //Arrange
+            ITrack track1 = Substitute.For<Track>("", 10, 10, 10, DateTime.Now);
+            ITrack track2 = Substitute.For<Track>("", 10, 10, 10, DateTime.Now);
+
+            ISeperationEvent evnt = Substitute.For<SeperationEvent>(track1, track2);
+            _uut.CurrEvents = Substitute.For<List<ISeperationEvent>>();
+            _uut.CurrEvents.Add(evnt);
+
+            //Act
+            _uut.EndEvent(evnt);
+
+            //Assert
+            Assert.That(_uut.CurrEvents.Count, Is.EqualTo(0));
+
+        }
+    }
+}
