e4123e65a10e9e3f10d01a4e99313f933f18f7f1
Anders Helboe Poulsen <au573698@uni.au.dk>
2018-11-19 16:00:32 +0100

    Added ExitEvent

---
+++ ATMPart1/ATMPart1/ExitEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMPart1
+{
+    public class ExitEvent : IEvent
+    {
+        public ITrack[] InvolvedTracks { get; }
+        public DateTime timeOfOccurence { get; }
+
+        public ExitEvent(ITrack track)
+        {
+            InvolvedTracks = new ITrack[1]{track};
+            timeOfOccurence = track.timestamp;
+        }
+        public void setTimeOfOccurence(DateTime time)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
