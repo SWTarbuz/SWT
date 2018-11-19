1984236988928caa7742717573554692041d6c5f
dilaksan bavarajan <dilaksun@hotmail.com>
2018-11-19 16:08:35 +0100

    added test for seperation event

---
+++ ATMPart1/ATMUnitTest/TestSeperationEvent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATMPart1;
+using NSubstitute;
+using NSubstitute.Core;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace ATMUnitTest
+{
+    class TestSeperationEvent
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+
+        public void TestSeperationEvent_SeperationEventOccurs_SetsToExpectedTime()
+        {
+            var time = DateTime.Now;
+            DateTime date1 = new DateTime(2013, 6, 1, 12, 32, 30);
+            var nTrack = Substitute.For<Track>("B342", 20000, 20000, 550f, date1);
+            var oTrack = Substitute.For<Track>("J443", 20000, 20000, 550f, time);
+
+            var se = new SeperationEvent(nTrack, oTrack);
+
+            Assert.That(se.timeOfOccurence, Is.EqualTo(date1));
+        }
+    }
+}
