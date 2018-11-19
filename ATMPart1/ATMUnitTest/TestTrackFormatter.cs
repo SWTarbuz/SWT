cc096a08cb06d27fe35d5707627238c6464f0d5f
Anders Helboe Poulsen <au573698@uni.au.dk>
2018-11-19 05:28:47 +0100

    Updated unit tests to reflect the new TrackFormat

--- ATMPart1/ATMUnitTest/TestTrackFormatter.cs
+++ ATMPart1/ATMUnitTest/TestTrackFormatter.cs
@@ -21,9 +21,9 @@ namespace ATMUnitTest
         public void TestRecieveTrack_validData_ReturnsExpectedTime() //format test to match the way  TestRecieveTrack_IllegalTimeString_ThrowsFormatException is set up.
         {
             //Arrange
-            var data = "tag;3.7;2000.5;5000;201712122000250";
-            string[] formats = { "yyyyMMddHHmmfff" };
-            var time = DateTime.ParseExact("201712122000250", formats[0], CultureInfo.CurrentCulture); ;
+            var data = "tag;3.7;2000.5;5000;20171212200012250";
+            string[] formats = { "yyyyMMddHHmmssfff" };
+            var time = DateTime.ParseExact("20171212200012250", formats[0], CultureInfo.CurrentCulture); ;
 
             var trackFormatter = new TrackFormatter();
 
@@ -37,7 +37,7 @@ namespace ATMUnitTest
         public void TestRecieveTrack_LegalData_ReturnsExpectedTag()
         {
             //Arrange
-            var data = "tag;3.7;2000.5;5000;201712122000250";
+            var data = "tag;3.7;2000.5;5000;20171212200012250";
 
             var trackFormatter = new TrackFormatter();
 
@@ -51,7 +51,7 @@ namespace ATMUnitTest
         public void TestRecieveTrack_TooLongDataString_ThrowsArgumentOutOfRangeException()
         {
             //Arrange
-            var data = "tag;3.7;2000.5;5000;201712122000250;forMeget";
+            var data = "tag;3.7;2000.5;5000;20171212200012250;forMeget";
 
             var trackFormatter = new TrackFormatter();
 
@@ -72,11 +72,12 @@ namespace ATMUnitTest
         }
 
         //technically really just a test of the dateTime class, as this is the one throwing the exception
-        [TestCase("201713122000251")] //13th month, not legal
-        [TestCase("2017121220002511")] //too long string, doens't match required format
-        [TestCase("201712122400251")] //24 hrs, when hrs are defined in 0-23 Hrs.
-        [TestCase("201712122061251")] //61 minutes
-        [TestCase("20100122061251")] //0th month
+        [TestCase("20171312200012251")] //13th month, not legal
+        [TestCase("201712122000122511")] //too long string, doens't match required format
+        [TestCase("20171212240012251")] //24 hrs, when hrs are defined in 0-23 Hrs.
+        [TestCase("20171212206112251")] //61 minutes
+        [TestCase("2010012206112251")] //0th month
+        [TestCase("20100122061251")] //To short a string
         public void TestRecieveTrack_IllegalTimeString_ThrowsFormatException(String dateString)
         {
             //Arrange
