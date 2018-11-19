9adee4f933a680c5f39c3899438ef3b870b7008b
Anders Helboe Poulsen <au573698@uni.au.dk>
2018-11-19 10:19:07 +0100

    Fized privates to match convention, and made ctor to initialize lists.

--- ATMPart1/ATMPart1/SeperationEventList.cs
+++ ATMPart1/ATMPart1/SeperationEventList.cs
@@ -6,26 +6,33 @@ using System.Threading.Tasks;
 
 namespace ATMPart1
 {
-    class SeperationEventList : ISeperationEventList
+    public class SeperationEventList : ISeperationEventList
     {
-        private List<ISeperationEvent> currEvents { get; set; }
-        private List<ISeperationEvent> prevEvents { get; set; }
+        private List<ISeperationEvent> _currEvents { get; set; }
+        private List<ISeperationEvent> _prevEvents { get; set; }
+
+        public SeperationEventList()
+        {
+            _currEvents = new List<ISeperationEvent>();
+            _prevEvents = new List<ISeperationEvent>();
+        }
+
 
         public IList<ISeperationEvent> CurrEvents
         {
-            get { return currEvents;}
-            set { currEvents = value.ToList(); }
+            get { return _currEvents;}
+            set { _currEvents = value.ToList(); }
         }
 
         public IList<ISeperationEvent> PrevEvents
         {
-            get { return prevEvents; }
+            get { return _prevEvents; }
         }
 
         //TODO: 99% sure that he didn't want us to make it update events, except for ending them. Thus further work on this isn't needed.
         public void UpdateCurrEvent(ISeperationEvent sepEvent)
         {
-            foreach (var evnt in currEvents)
+            foreach (var evnt in _currEvents)
             {
                 if (evnt.InvolvedTracks == sepEvent.InvolvedTracks)
                 {
@@ -37,8 +44,8 @@ namespace ATMPart1
 
         public void EndEvent(ISeperationEvent sepEvent)
         {
-            currEvents.Remove(sepEvent);
-            prevEvents.Add(sepEvent);
+            _currEvents.Remove(sepEvent);
+            _prevEvents.Add(sepEvent);
         }
     }
 }
