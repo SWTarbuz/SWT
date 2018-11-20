25a906e3089c03f3fd3f9ea31395139ddc388e62
dilaksan bavarajan <dilaksun@hotmail.com>
2018-11-20 16:24:37 +0100

    Test cases for EntryEvent and ExitEvent

--- ATMPart1/ATMUnitTest/TestEntryEvent.cs
+++ ATMPart1/ATMUnitTest/TestEntryEvent.cs
@@ -3,11 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using ATMPart1;
 
 namespace ATMUnitTest
 {
     class TestEntryEvent
     {
 
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+
+        [Test]
+        public void testEntryEvent_CreateEntryEvent_TimeGetsSet()
+        {
+            var time = DateTime.Now;
+            var trak = Substitute.For<Track>("tag", 20000, 20000, 550f, time);
+
+            IEvent entryEvent = new EntryEvent(trak);
+
+            Assert.That(entryEvent.timeOfOccurence, Is.EqualTo(trak.timestamp));
+        }
+
+        [Test]
+        public void testEntryEvent_CreateEntryEvent_InvolvedTracksGetsSet()
+        {
+            var time = DateTime.Now;
+            var trak = Substitute.For<Track>("tag", 20000, 20000, 550f, time);
+
+            IEvent entryEvent = new EntryEvent(trak);
+
+            Assert.That(entryEvent.InvolvedTracks.Count, Is.EqualTo(1));
+            Assert.That(entryEvent.InvolvedTracks[0].tag, Is.EqualTo(trak.tag));
+        }
+
+        [Test]
+        public void testEntryEvent_Print_CorrectStringReturned()
+        {
+            var time = DateTime.Now;
+            var trak = Substitute.For<Track>("tag", 20000, 20000, 550f, time);
+
+            IEvent entryEvent = new EntryEvent(trak);
+
+            Assert.That(entryEvent.Print, Is.EqualTo($"at the time of: {entryEvent.timeOfOccurence}, the track: {entryEvent.InvolvedTracks[0].tag}, Entered the Airspace"));
+
+        }
     }
 }
---
+++ ATMPart1/ATMUnitTest/TestExitEvent.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using ATMPart1;
+
+namespace ATMUnitTest
+{
+    class TestExitEvent
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+
+        [Test]
+        public void testExitEvent_CreateExitEvent_TimeGetsSet()
+        {
+            var time = DateTime.Now;
+            var trak = Substitute.For<Track>("tag", 100000, 20000, 550f, time);
+
+            IEvent exitEvent = new ExitEvent(trak);
+
+            Assert.That(exitEvent.timeOfOccurence, Is.EqualTo(trak.timestamp));
+        }
+
+        [Test]
+        public void testExitEvent_CreateExitEvent_InvolvedTracksGetsSet()
+        {
+            var time = DateTime.Now;
+            var trak = Substitute.For<Track>("tag", 100000, 20000, 550f, time);
+
+            IEvent exitEvent = new ExitEvent(trak);
+
+            Assert.That(exitEvent.InvolvedTracks.Count, Is.EqualTo(1));
+            Assert.That(exitEvent.InvolvedTracks[0].tag, Is.EqualTo(trak.tag));
+        }
+
+        [Test]
+        public void testExitEvent_Print_CorrectStringReturned()
+        {
+            var time = DateTime.Now;
+            var trak = Substitute.For<Track>("tag", 90001, 20000, 550f, time);
+
+            IEvent exitEvent = new ExitEvent(trak);
+
+            Assert.That(exitEvent.Print, Is.EqualTo($"at the time of: {exitEvent.timeOfOccurence}, the track: {exitEvent.InvolvedTracks[0].tag}, Left the Airspace"));
+
+        }
+    }
+}
