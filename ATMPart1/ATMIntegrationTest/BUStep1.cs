8ed4a7a370312cb701e402bca252441b05baec45
dilaksan bavarajan <dilaksun@hotmail.com>
2018-11-20 14:47:59 +0100

    Added first integrationstep test functions

--- ATMPart1/ATMIntegrationTest/BUStep1.cs
+++ ATMPart1/ATMIntegrationTest/BUStep1.cs
@@ -16,18 +16,54 @@ namespace ATMIntegrationTest
 
         private Track track;
         private Airspace airspace;
-        private SeperationEvent seperationEvent;
 
         private TrackManager tm;
-        private TrackFormatter tf;
-        private EventList seList;
+
 
         [SetUp]
         public void SetUp()
         {           
-            track = new Track("", 0, 0, 0, DateTime.Now);
+
             airspace = new Airspace(10000, 90000, 10000, 90000, 500, 2000);
 
+            tm = new TrackManager();
         }
+
+
+
+
+        [Test]
+        public void TrackManager_HandleTrack_OutOfAirSpace()
+        {
+            track = new Track("B837", 0, 0, 0, DateTime.Now);
+            tm.HandleTrack(track, airspace);
+
+            Assert.That(tm.Tracks.Count, Is.EqualTo(0));
+        }
+    
+        [Test]
+        public void TrackManager_HandleTrack_EntersAirSpace()
+        {
+            track = new Track("B837", 0, 0, 0, DateTime.Now);
+            var track2 = new Track("B837", 20000, 20000, 700, DateTime.Now);
+            tm.HandleTrack(track, airspace);
+            Assert.That(tm.Tracks.Count, Is.EqualTo(0));
+            track = track2;
+            tm.HandleTrack(track, airspace);
+            Assert.That(tm.Tracks.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TrackManager_HandleTrack_ExitsAirSpace()
+        {
+            track = new Track("B837", 20000, 20000, 700, DateTime.Now);
+            var track2 = new Track("B837", 0, 0, 0, DateTime.Now);
+            tm.HandleTrack(track, airspace);
+            Assert.That(tm.Tracks.Count, Is.EqualTo(1));
+            track = track2;
+            tm.HandleTrack(track, airspace);
+            Assert.That(tm.Tracks.Count, Is.EqualTo(0));
+        }
+
     }
 }
