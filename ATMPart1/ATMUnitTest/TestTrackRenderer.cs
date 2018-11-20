b2b5d331d28c1c56339662f6aa3674c3bb358cb2
Anders Helboe Poulsen <anders@bjergtoppen.dk>
2018-11-20 16:49:17 +0100

    Added TestTrackRenderer, Although it seems untestable.

---
+++ ATMPart1/ATMUnitTest/TestTrackRenderer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+using ATMPart1;
+using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace ATMUnitTest
+{
+    public class TestTrackRenderer
+    {
+        private ITrack _track;
+        private List<ITrack> _tracks;
+
+        private ITrackManager _tm;
+        private IEventList _el;
+        private ITrackRenderer _uut;
+
+        private int _eventsRecieved;
+        private TracksUpdatedEventArgs _RecievedArgs;
+
+        [SetUp]
+        public void Setup()
+        {
+            _track = Substitute.For<ITrack>();
+            _tracks = Substitute.For<List<ITrack>>();
+            _tracks.Add(_track);
+
+            _eventsRecieved = 0;
+
+            _tm = Substitute.For<ITrackManager>();
+            _el = Substitute.For<IEventList>();
+            _uut = new TrackRenderer(_tm, _el);
+        }
+
+        //TODO: Can this even be tested? As everything is private, and it doesn't send any events out.
+        //[Test]
+        //public void TestHandleTrackUpdate_EventSent_RecievedEvent()
+        //{
+        //    TracksUpdatedEventArgs sentArgs = new TracksUpdatedEventArgs(_tracks, _track);
+
+        //    _tm.RaiseTracksUpdatedEvent += Raise.EventWith(_tm, sentArgs);
+
+            
+        //    Assert.That();
+        //}
+    }
+}
