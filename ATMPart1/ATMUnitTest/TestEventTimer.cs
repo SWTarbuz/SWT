3abfd7de0cbdbc9a9e00e411dae63947e4f78f4a
Anders Helboe Poulsen <anders@bjergtoppen.dk>
2018-11-21 06:14:54 +0100

    Attempted fix at the tests of EventTimer, old code is outcommented.

--- ATMPart1/ATMUnitTest/TestEventTimer.cs
+++ ATMPart1/ATMUnitTest/TestEventTimer.cs
@@ -13,6 +13,7 @@ namespace ATMUnitTest
     //TODO: consider injection of the timer, as it is a dependency
     public class TestEventTimer
     {
+        private FakeSubscriber _sub;
         private IEvent _evnt;
         private EventTimer _uut;
         private IEvent _outEvent;
@@ -23,6 +24,8 @@ namespace ATMUnitTest
         {
             _eventsReceived = 0;
             _evnt = Substitute.For<IEvent>();
+
+            _sub = Substitute.For<FakeSubscriber>();
         }
 
         //MethodUnderTest_Scenario_ExpectedBehaviour
@@ -33,14 +36,18 @@ namespace ATMUnitTest
         public void EventTimer_xTime_RaiseTimerOccuredEventAfterxTime(int time)
         {
             _uut = new EventTimer(_evnt, time);
-            _uut.RaiseTimerOccuredEvent += (o, args) =>
-            {
-                _outEvent = args.Evnt;
-                ++_eventsReceived;
-            };
+            //_uut.RaiseTimerOccuredEvent += (o, args) =>
+            //{
+            //    _outEvent = args.Evnt;
+            //    ++_eventsReceived;
+            //};
+
+            _uut.RaiseTimerOccuredEvent += _sub.EventSub;
 
             System.Threading.Thread.Sleep(time+20); //Adds a little bit to ensure that the timer has time to call the event
-            Assert.That(_eventsReceived, Is.EqualTo(1));
+            //Assert.That(_eventsReceived, Is.EqualTo(1));
+            //_sub.Received().EventSub(Arg.Any<Object>(), Arg.Any<TimerForEventOccuredEventArgs>());
+            Assert.That(_sub.Cnt, Is.EqualTo(1));
         }
 
         //TODO: These run perfectly fine when ran alone, but when together they fail.
@@ -56,9 +63,23 @@ namespace ATMUnitTest
                 _outEvent = args.Evnt;
                 ++_eventsReceived;
             };
-            
+            _uut.RaiseTimerOccuredEvent += _sub.EventSub;
+
             System.Threading.Thread.Sleep(time -10);
-            Assert.That(_eventsReceived, Is.EqualTo(0));
+            //Assert.That(_eventsReceived, Is.EqualTo(0));
+            Assert.That(_sub.Cnt, Is.EqualTo(0));
+        }
+    }
+
+    //Using our own fake here due to an issue where our count on the test class had issues when running multiple tests.
+    public class FakeSubscriber
+    {
+        public int Cnt = 0;
+
+        //testing of event by a subscriber that we can call .Recieved on
+        public void EventSub(object source, TimerForEventOccuredEventArgs e)
+        {
+            Cnt++;
         }
     }
 }
