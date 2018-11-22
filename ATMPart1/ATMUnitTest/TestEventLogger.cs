49d6e849acbe0188cc2479c10866db3c09db4097
Anders Helboe Poulsen <anders@bjergtoppen.dk>
2018-11-22 08:37:47 +0100

    Added testfile for EventLogger without any tests.

---
+++ ATMPart1/ATMUnitTest/TestEventLogger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using ATMPart1;
+
+
+namespace ATMUnitTest
+{
+    public class TestEventLogger
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        //[Test]
+        //public void LogEventToFile_EntryEvent_StreamWriterCalled()
+        //{
+
+        //}
+    }
+}
