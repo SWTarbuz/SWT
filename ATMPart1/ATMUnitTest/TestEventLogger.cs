c4cb5da126a70dbe3d01e829e1b4e4f50401d3f4
Anders Helboe Poulsen <anders@bjergtoppen.dk>
2018-11-22 09:15:36 +0100

    Updated EventLogger with internal TextWriter and made test of it.

--- ATMPart1/ATMUnitTest/TestEventLogger.cs
+++ ATMPart1/ATMUnitTest/TestEventLogger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -8,22 +9,28 @@ using System.Threading.Tasks;
 using NSubstitute;
 using NUnit.Framework;
 using ATMPart1;
+using WrapThat.SystemIO;
 
 
 namespace ATMUnitTest
 {
     public class TestEventLogger
     {
+        private TextWriter _writer;
+
         [SetUp]
         public void Setup()
         {
-            
+            EventLogger.Writer = Substitute.For<TextWriter>();
         }
 
-        //[Test]
-        //public void LogEventToFile_EntryEvent_StreamWriterCalled()
-        //{
+        [TestCase("Any string")]
+        [TestCase("EntryEvent track: xyz, at the time of 21:11:23")]
+        public void LogEventToFile_WithEventText_StreamWriterCalledWithArgEventText(string eventText)
+        {
+            EventLogger.LogEventToFile(eventText);
 
-        //}
+            EventLogger.Writer.Received().WriteLine(Arg.Is(eventText));
+        }
     }
 }
