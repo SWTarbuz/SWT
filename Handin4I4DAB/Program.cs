4851dbeecf88d7a5daefd6b508a53edddb2cc209
Anders Helboe Poulsen <anders@bjergtoppen.dk>
2018-12-05 13:40:31 +0100

    Made Solution for I4DAb faget, with a project for the models

---
+++ Handin4I4DAB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Handin4I4DAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
